35365a45a218b281b6f4e5e3ebb0faaced6a1149
duongtram1702 <duong170226@gmail.com>
2025-03-06 11:50:44 +0700

    submit

--- Session 22/Ss22-7/main.js
+++ Session 22/Ss22-7/main.js
@@ -1,5 +1,14 @@
 let a = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 2, 8, 3, 1, 4, 4];
-a.sort((a, b) => a - b);
+let n = a.length;
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+        if (a[j] > a[j + 1]) {
+            let temp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = temp;
+        }
+    }
+}
 for (let i in a) {
     document.write(`${a[i]} `);
 }
