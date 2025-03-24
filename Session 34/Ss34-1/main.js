c25c43b18d1e5396af461173def735c1061c0482
duongtram1702 <duong170226@gmail.com>
2025-03-25 03:53:58 +0700

    submit

---
+++ Session 34/Ss34-1/main.js
@@ -0,0 +1,67 @@
+let storeAccount = [
+    { email: 'admin170226@gmail.com', password: 'admin170226' },
+];
+let noti = document.getElementById('noti-duplicate');
+function save() {
+    localStorage.setItem('storeAccount', JSON.stringify(storeAccount));
+}
+save();
+function validateEmail(email) {
+    if (!email) {
+        noti.innerText = 'Email cannot be empty!';
+        noti.style.display = 'flex';
+        return false;
+    }
+
+    if (!email.includes('@')) {
+        noti.innerText = "Email must contain '@'!";
+        noti.style.display = 'flex';
+        return false;
+    }
+
+    if (!email.endsWith('.vn') && !email.endsWith('.com')) {
+        noti.innerText = "Email must end with '.vn' or '.com'!";
+        noti.style.display = 'flex';
+        return false;
+    }
+    return true;
+}
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    let email = document.getElementById('email').value;
+    if (!validateEmail(email)) return;
+
+    let checkPassword = document.getElementById('password-not-match');
+    let found = storeAccount.find((acc) => acc.email === email);
+    if (found) {
+        noti.style.display = 'flex';
+        return;
+    }
+    noti.style.display = 'none';
+
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('password-confirm').value;
+
+    if (!password || !confirmPassword) {
+        checkPassword.innerText =
+            'Password or confirm password cannot be empty!';
+        checkPassword.style.display = 'flex';
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        checkPassword.style.display = 'flex';
+        return;
+    }
+    checkPassword.style.display = 'none';
+
+    storeAccount.push({ email: email, password: password });
+    checkPassword.innerText = 'Register successfully !';
+    checkPassword.style.color = 'rgb(65, 244, 71)';
+    checkPassword.style.display = 'flex';
+    save();
+    setTimeout(() => {
+        window.location.href = 'home.html';
+    }, 1000);
+});
